5287378f924c74d6ce12ae4ca451103f21bfef10
Devin Ferko <devin.ferko@outlook.com>
2025-10-15 15:09:09 +0100

    Script name change and simple plot complete

--- apps/Iris Dataset/Iris - Simple 3D Scatter - Plotly Express.py
+++ apps/Iris Dataset/Iris - Simple 3D Scatter - Plotly Express.py
@@ -1,5 +1,6 @@
 # Imports Libraries
 import pandas as pd
+import plotly.express as px
 
 # Load the Iris dataset from URL
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
@@ -7,3 +8,6 @@ iris_df = pd.read_csv(url)
 
 # Check dataset - comment/uncomment when needed
 print(iris_df.head())
+
+fig = px.scatter_3d(iris_df, x='sepal_length', y='sepal_width', z='petal_length', color='species')
+fig.show()
