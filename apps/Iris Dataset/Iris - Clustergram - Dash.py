2377f440d4f0245e3710cc6ba40c2bf4b20cf15d
Devin Ferko <devin.ferko@outlook.com>
2025-10-16 10:51:41 +0100

    Initial start and read csv

---
+++ apps/Iris Dataset/Iris - Clustergram - Dash.py
@@ -0,0 +1,15 @@
+# Imports Libraries and Dependencies
+import pandas as pd
+from dash import Dash, dcc, html, Input, Output, callback
+import dash_bio as dashbio
+
+# Initializes the Dash app
+app = Dash()
+
+# Reads in the Iris Dataset from a URL
+iris_df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv").set_index("species")
+
+# Extracts the column and row names from the dataframe
+columns = list(iris_df.columns.values)
+rows = list(iris_df.index)
+
