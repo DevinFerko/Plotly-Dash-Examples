8f6d423c7cc4edfe5fdee890b7c945e08b82ac8a
Devin Ferko <devin.ferko@outlook.com>
2025-10-27 10:40:02 +0000

    Some typos fixed and Color mapping added for plot (needed)

--- apps/Iris Dataset/Iris - Parallel Coordinates - Plotly Express.py
+++ apps/Iris Dataset/Iris - Parallel Coordinates - Plotly Express.py
@@ -6,8 +6,22 @@ import plotly.express as px
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
 iris_df = pd.read_csv(url)
 
+# Encode species for color mapping
+species_coded = {'setosa': 0, 'versicolor': 1, 'virginica': 2}
+iris_df['species_code'] = iris_df['species'].map(species_coded)
+
 # Parallel Coordinates Plot
-fig - px.parallel_coordinates(iris_df, color='species_id', labels={"species_id": "Species"},
+fig = px.parallel_coordinates(iris_df, 
+                            color="species_code", 
+                            labels={
+                                "species": "Species",
+                                'species_code': 'Species Code',
+                                'sepal_length': 'Sepal Length',
+                                'sepal_width': 'Sepal Width',
+                                'petal_length': 'Petal Length',
+                                'petal_width': 'Petal Width'
+                            },
                             color_continuous_scale=px.colors.diverging.Tealrose,
                             dimensions=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+
 fig.show()
