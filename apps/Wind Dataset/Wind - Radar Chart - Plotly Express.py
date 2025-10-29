03ab3413ff9c1caa9b80790e863d9665d383c960
Devin Ferko <devin.ferko@outlook.com>
2025-10-29 10:07:35 +0000

    Changed dataset and import

--- apps/Wind Dataset/Wind - Radar Chart - Plotly Express.py
+++ apps/Wind Dataset/Wind - Radar Chart - Plotly Express.py
@@ -1,8 +1,15 @@
-# Imports Libraries
+# Imports libraries
+import mlcroissant as mlc
 import pandas as pd
 import plotly.express as px
 
-# Load the Wind dataset from URL
-url = "https://raw.githubusercontent.com/datasets/wind/master/data/wind.csv"
-wind_df = pd.read_csv(url)
-wind_df.head()
+# Fetch the Croissant JSON-LD
+croissant_dataset = mlc.Dataset('https://www.kaggle.com/datasets/mubashirrahim/wind-power-generation-data-forecasting/croissant/download')
+
+# Check what record sets are in the dataset
+record_sets = croissant_dataset.metadata.record_sets
+print(record_sets)
+
+# Fetch the records and put them in a DataFrame
+record_set_df = pd.DataFrame(croissant_dataset.records(record_set=record_sets[0].uuid))
+record_set_df.head()
