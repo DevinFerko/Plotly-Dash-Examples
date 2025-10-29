f6dff337fec32e5e0b1fd16228269684de6cfecd
Devin Ferko <devin.ferko@outlook.com>
2025-10-29 15:16:20 +0000

    Cleaned up column headers and write to csv

--- apps/Wind Dataset/Wind - Radar Chart - Plotly Express.py
+++ apps/Wind Dataset/Wind - Radar Chart - Plotly Express.py
@@ -11,5 +11,10 @@ record_sets = croissant_dataset.metadata.record_sets
 print(record_sets)
 
 # Fetch the records and put them in a DataFrame
-record_set_df = pd.DataFrame(croissant_dataset.records(record_set=record_sets[0].uuid))
-record_set_df.head()
+wind_df = pd.DataFrame(croissant_dataset.records(record_set=record_sets[0].uuid))
+
+# Cleans up column names
+wind_df.columns = wind_df.columns.str.replace(r'^.*?/', '', regex=True)
+
+# creates csv file - uncomment if needed
+wind_df.to_csv('wind_data.csv', index=False)
